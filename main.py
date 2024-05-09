b9fa4484ae2c71ec885dfad733f5517dbb7f9f9d
Aerospacerr <huseyinemircan@gmail.com>
2024-05-09 13:34:26 +0200

    second

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 from typing import Union
 
 from fastapi import FastAPI
+from PIL import Image
+import io
 
 app = FastAPI()
 
@@ -14,6 +16,13 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+"""@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}"""
+
+
+@app.post("/ask")
+def ask(text: str, image: UploadFile):
+    content = image.file.read()
+
+    image = Image(io.BytesIO(content))
